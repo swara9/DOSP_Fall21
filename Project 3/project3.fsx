ad2cc22f278504e673e01038ef321a95bc2089a4
Swara Lawande <swara.lawande9@gmail.com>
2021-10-30 00:37:34 -0400

    added logic for getting closest preceeding node

--- Project 3/project3.fsx
+++ Project 3/project3.fsx
@@ -8,7 +8,7 @@ open System.Security.Cryptography
 open System.Collections.Generic
 
 let num_nodes = 10
-let num_message = 10
+let num_message = 5
 let hash_length = 160
 
 let chord_system = System.create "chord-system" (Configuration.load())
@@ -56,20 +56,49 @@ let ranStr n =
     String(Array.init n (fun _ -> chars.[r.Next sz]))
 
 let Chord_Node (mailbox : Actor<_>) =
-    // let mutable finger_table = Array2D.init hash_length 2 (fun _ _ -> "")
-    let mutable finger_table = new List<string>()
+    let mutable finger_table = Array2D.init hash_length 2 (fun _ _ -> "")
+    // let mutable finger_table = new List<string>()
     let mutable predecessor = ""
     let selfName = mailbox.Context.Self.Path.Name
 
     let find_closest_preceeding_node id =
         let mutable index = hash_length-1
-        //REMOVE
-          
-        while id <= finger_table.[index] do
-            index <- index - 1
-            printfn "currIndex at last %i" index
+        if id < selfName then
+            while finger_table.[index, 0] > id && index <> 0 do
+                printfn "currIndex case 0 %i" index
+                index <- (index-1)
+            if index = 0 then
+                finger_table.[(hash_length-1), 1]
+            else
+                finger_table.[index, 1]                 
+        
+            
+        else if finger_table.[index, 1] < selfName then
+            while finger_table.[index, 0]< id && index <> 0 do
+                printfn "currIndex case 1 %i" index
+                index <- (index - 1)
+            if index <> 0 then
+                while finger_table.[index, 0]> id do
+                    printfn "currIndex case 2 %i" index
+                    index <- (index - 1)
+            else
+                index <- (hash_length-1)
+            finger_table.[index, 1]
 
-        finger_table.[index]
+        else
+            while finger_table.[index, 0]> id do
+                printfn "currIndex case 3 %i" index
+                index <- (index - 1)
+            finger_table.[index, 1]
+
+
+
+        // if whole table traversed
+        // if index = 0 then
+        //     while id < finger_table.[index, 0] do
+        //     index <- index - 1
+        //     printfn "currIndex at last %i" index
+        // finger_table.[index, 1]
                 
     let basePath = "akka://chord-system/user/supervisor/"
     
@@ -91,16 +120,16 @@ let Chord_Node (mailbox : Actor<_>) =
                 else
                     next_largest <- chord_nodes.[binary_search chord_nodes.[..current_index] finger_int 0]
                 
-                // let mutable finger = finger_int.ToString("x2")
-                // if finger.Length = 40 then
-                //     finger <- "0" + finger
+                let mutable finger = finger_int.ToString("x2")
+                if finger.Length = 40 then
+                    finger <- "0" + finger
                 let mutable finger_val = next_largest.ToString("x2")
                 if finger_val.Length = 40 then
                     finger_val <- "0" + finger_val
                 //let mutable path = "akka://chord-system/user/supervisor/"+finger_val
-                // finger_table.[i-1, 0] <- finger
-                // finger_table.[i-1] <- finger_val
-                finger_table.Add(finger_val)
+                finger_table.[i-1, 0] <- finger
+                finger_table.[i-1, 1] <- finger_val
+                // finger_table.Add(finger_val)
                 // finger_table.[i-1, 2] <- ""
 
             mailbox.Sender() <! Init_Done
@@ -108,18 +137,19 @@ let Chord_Node (mailbox : Actor<_>) =
         | Begin_Simulation ->
             for i in 1 .. num_message do
                 //generate random message and hash it
-                let randomMsg = ranStr(10) 
+                let randomMsg = ranStr(50) 
                 let hashedMsg = hash_string(randomMsg, hash_type)  
                 //REMOVE
                 //if key already in successor
-                if hashedMsg > predecessor && hashedMsg <= selfName then
+                if (selfName < predecessor && (hashedMsg > predecessor || hashedMsg < selfName)) || (hashedMsg > predecessor && hashedMsg <= selfName) then
                     //REMOVE
                     // printfn "Converging message"
                     mailbox.Context.Parent <! Received_Message(0)
                 else
                     //if successor has key
-                    if hashedMsg > selfName && hashedMsg <= finger_table.[0] then
-                        let path = basePath+finger_table.[0]
+                    let successor = finger_table.[0,1]
+                    if (hashedMsg > selfName && hashedMsg <= successor) || (successor< selfName && (hashedMsg> selfName || hashedMsg< successor)) then
+                        let path = basePath+successor
                         let actorRef = select path chord_system 
                         actorRef <! Route(hashedMsg, 0) 
                     else
@@ -132,16 +162,16 @@ let Chord_Node (mailbox : Actor<_>) =
 
         | Route(hashedMsg, hop) ->
             let mutable num_hops = hop + 1
-
             //if message lesser than or equal to successor
-            if hashedMsg > predecessor && hashedMsg <= selfName then
+            if (selfName < predecessor && (hashedMsg > predecessor || hashedMsg < selfName))  || (hashedMsg > predecessor && hashedMsg <= selfName) then
                 //REMOVE
                 // printfn "Converging message"
                 mailbox.Context.Parent <! Received_Message(num_hops)
             else
                 //if successor has key
-                if hashedMsg > selfName && hashedMsg <= finger_table.[0] then
-                    let path = basePath+finger_table.[0]
+                let successor = finger_table.[0, 1]
+                if (hashedMsg > selfName && hashedMsg <= successor) || (successor< selfName && (hashedMsg> selfName || hashedMsg< successor)) then
+                    let path = basePath+successor
                     let actorRef = select path chord_system 
                     actorRef <! Route(hashedMsg, num_hops) 
                 else
@@ -182,10 +212,10 @@ let Supervisor (mailbox : Actor<_>) =
         | Init_Done ->  init_count <- init_count + 1
                         if init_count = node_count then
                             printfn "Chord ring created"
-                            // chord_list |> Seq.iteri (fun i chord_node -> 
-                            //         chord_node <! Begin_Simulation)
-                            chord_list.[9] <! Begin_Simulation
-                            chord_list.[6] <! Begin_Simulation
+                            chord_list |> Seq.iteri (fun i chord_node -> 
+                                    chord_node <! Begin_Simulation)
+                            // chord_list.[9] <! Begin_Simulation
+                            // chord_list.[6] <! Begin_Simulation
 
 
         | Received_Message(hops) ->
