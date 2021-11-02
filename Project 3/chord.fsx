512f94eb734b4d1477a68be0b00357745042bc5f
Swara Lawande <swara.lawande9@gmail.com>
2021-11-02 00:51:25 -0400

    Chord with stabilize

--- Project 3/chord.fsx
+++ Project 3/chord.fsx
@@ -12,19 +12,18 @@ let num_message = 10
 let hash_length = 160
 let chord_system = System.create "chord-system" (Configuration.load())
 let maxNode:bigint = bigint(2.0 ** 160.0)
+let mutable noActorsJoined = 0
 
 type Supervisor_Messge = 
     | Start of nodes:int * messages:int
-    | CreateNewNode of node: string
-    | Init_Done
+    | CreateOtherNodes
     | Received_Message of no_of_hops: int
 
 type Node_Message =
-    | Initialize of chord_nodes: bigint [] * current_index: int
     // | Begin_Simulation
     // | Route of message: string * hop: int
-    | FindSuccessor of id: string * askerNode: bigint * purpose: string
-    | ChangeSuccessor of succesor: string
+    | FindSuccessor of id: string * askerNode: string * purpose: int
+    | ChangeSuccessor of successor: string
     | Create
     | Join of node: string
     | Stabilize of predOfSucc: string
@@ -33,16 +32,18 @@ type Node_Message =
     | CheckPred of state : string
     | TellState
     | FixFingers
-    | UpdateFinger of value: bigint * index: int 
+    | UpdateFinger of value: string * index: int 
+
 let hash_type = "SHA1"
 
-let hash_string (input: string, algo: string) =
+let hash_string (input: string) =
+    let algo = hash_type
     let hash_bytes = input 
                         |> System.Text.Encoding.UTF8.GetBytes
                         |> HashAlgorithm.Create(algo).ComputeHash
     let hash_string = "0" + 
                         (hash_bytes
-                        |> Seq.map (fun c -> c.ToString "x2")
+                        |> Seq.map (fun c -> c.ToString "X")
                         |> Seq.reduce (+))
     hash_string
 
@@ -56,153 +57,136 @@ let ranStr n =
 let convertToBigInt hash =
     bigint.Parse(hash, System.Globalization.NumberStyles.HexNumber)
 
-let Chord_Node (mailbox : Actor<_>) =
+let chordNode (mailbox : Actor<_>) =
     // let mutable finger_table = Array2D.init hash_length 2 (fun _ _ -> "")
-    let mutable finger_table = new List<string>()
-    let mutable predecessor:bigint = -1.0 |>bigint
+    let mutable finger_table = Array.create hash_length ""
+    let mutable predecessor = ""
     let currNode = mailbox.Context.Self.Path.Name
     let basePath = "akka://chord-system/user/supervisor/"
     let mutable state = "active"
     let mutable next = 1
     let nodeInt:bigint = convertToBigInt currNode
 
-
-    let getActorRef id:bigint =
-        // let hashID = id |> bigint |> ToString("x2")
-        let hashID = id.ToString  "X"
-        let path = basePath+hashID
+    let getActorRef id =
+        let path = basePath+id
         let actorRef = select path chord_system
         actorRef
-
-
-    let get_next_node id = 
-        let mutable index = hash_length-1
-        while id < finger_table.[index] do
-               index <- (index-1)
-               //REMOVE
-            //    printfn "table index %i for %s" index currNode
-        
-        finger_table.[index]
-
+   
     let find_closest_preceeding_node id =
         let mutable index = hash_length-1
+        while finger_table.[index] = "" do
+            index <- (index-1)
+
         //if we have to crossover
-        if id < nodeInt then
-            if finger_table.[index] > nodeInt then
+        if id < currNode then
+            if finger_table.[index] > currNode then
+                printfn "%s --- index %i case 0" currNode index              
                 finger_table.[index]
-                // printfn "index %i" index              
             else
-                while id < finger_table.[index] && finger_table.[index-1]<= finger_table.[index] do 
+                while (id < finger_table.[index] && finger_table.[index-1]<= finger_table.[index]) do 
                     index <- (index-1)
-                    // printfn "index %i" index
+                    printfn "index %i case 1" index
                 if finger_table.[index-1]> finger_table.[index] && id< finger_table.[index] then
                     index <- (index-1)
-                    // printfn "index %i" index
+                    printfn "index %i case 2" index
                 finger_table.[index]
         else
             //it has crossed over which we don't want
-            if finger_table.[index] < nodeInt then
-                while finger_table.[index-1] <= finger_table.[index] do
+            if finger_table.[index] < currNode || finger_table.[index] = "" then                
+                while finger_table.[index-1] <= finger_table.[index] || index >1 do
                     index <- (index-1)
-                    printfn "index %i" index              
+                    printfn "index %i case 3" index              
                 index <- (index-1)
-            while id < finger_table.[index] do
-               index <- (index-1)
-               printfn "index %i" index       
-            finger_table.[index]
-                
+            if index = 0 then
+                finger_table.[index]
+            else
+                while id < finger_table.[index] do
+                   index <- (index-1)
+                   printfn "index %i case 4" index       
+                finger_table.[index]
+                    
     
     let rec loop () = actor {
         let! message = mailbox.Receive()
         match message with
-        | Initialize(chord_nodes, current_index) -> 
-            let total_nodes = chord_nodes.Length
-            let current_node_int = chord_nodes.[current_index]
-            predecessor <- chord_nodes.[(total_nodes + current_index - 1) % total_nodes].ToString("x2")
-            if predecessor.Length = 40 then
-                predecessor <- "0" + predecessor
-            let chord_size =  bigint (2.0**160.0)
-            for i in 1 .. 160 do
-                let raw_value = current_node_int + bigint (2.0 ** float (i-1))
-                let finger_int = raw_value % chord_size
-                let mutable next_largest = bigint 0
-                let offset = 0
-                if finger_int > current_node_int then
-                    next_largest <- chord_nodes.[binary_search chord_nodes.[(current_index+1)%total_nodes..] finger_int (current_index+1)%total_nodes]
-                else
-                    next_largest <- chord_nodes.[binary_search chord_nodes.[..current_index] finger_int 0]
-                let mutable finger_val = next_largest.ToString("x2")
-                if finger_val.Length = 40 then
-                    finger_val <- "0" + finger_val
-                finger_table.[i-1] <- finger_val
-               
-            mailbox.Sender() <! Init_Done
-
         | FindSuccessor(id, asker, purpose) ->             
             let successor = finger_table.[0]
-            if (id > nodeInt && id <= successor) || (successor< nodeInt && (id> nodeInt || id<= successor)) then
-                //means join
-                if purpose = -1 then
-                    let actorRef = getActorRef id 
-                    actorRef <! ChangeSuccessor
-                //means update finger table
-                else 
+            if currNode = successor && purpose = -1 then
+                let actorRef = getActorRef asker
+                actorRef <! ChangeSuccessor(successor)
+                mailbox.Self <! ChangeSuccessor(asker)
+            else 
+                if (id > currNode && id <= successor) || (successor< currNode && (id> currNode || id<= successor)) then
                     let actorRef = getActorRef asker
-                    actorRef <! UpdateFinger(id, purpose)                 
-            else
-                let cpn = find_closest_preceeding_node id
-                let actorRef = getActorRef cpn
-                actorRef <! FindSuccessor(id, asker, purpose)
+                    //means join
+                    if purpose = -1 then
+                        noActorsJoined <- noActorsJoined+1
+                        actorRef <! ChangeSuccessor(successor)                        
+                    //means update finger table
+                    else 
+                        actorRef <! UpdateFinger(id, purpose)                 
+                else
+                    let cpn = find_closest_preceeding_node id
+                    let actorRef = getActorRef cpn
+                    actorRef <! FindSuccessor(id, asker, purpose)
 
         | ChangeSuccessor(successor) ->
             finger_table.[0] <- successor
 
         | Create ->
             printfn "Create called"
-            predecessor <- -1
-            finger_table.[0] <- nodeInt
+            noActorsJoined <- 1
+            predecessor <- ""
+            finger_table.[0] <- currNode
+            mailbox.Sender() <! CreateOtherNodes
 
         | Join(existingNode) ->
             printfn "Join Called"
-            predecessor <- -1
+            predecessor <- ""
             let actorRef = getActorRef(existingNode)
-            actorRef <! FindSuccessor(nodeInt, nodeInt, -1)
+            actorRef <! FindSuccessor(currNode, currNode, -1)
 
         | TellYourPred ->
-            mailbox.Sender <! Stabilize(predecessor)
+            // printfn "Calling stabilize"
+            mailbox.Sender() <! Stabilize(predecessor)
 
         | Stabilize(x) ->
-            if x <> -1 then
+            if x <> "" then
                 // crossover
-                if(finger_table.[0] < nodeInt) then
-                    if x < finger_table.[0] || x > nodeInt then
+                if(finger_table.[0] < currNode) then
+                    if x < finger_table.[0] || x > currNode then
                         finger_table.[0] <- x
-                else if (x > nodeInt && x < finger_table.[0]) then
+                else if (x > currNode && x < finger_table.[0]) then
                     finger_table.[0] <- x
             let actorRef = getActorRef finger_table.[0]
-            actorRef <! Notify(nodeInt)
+            actorRef <! Notify(currNode)
 
         | Notify(pred) ->
-            if (predecessor = -1) || (pred> nodeInt && (pred > predecessor || pred < selfName)) || (pred> predecessor && pred< nodeInt) then
+            if (predecessor = "") || (pred> currNode && (pred > predecessor || pred < currNode)) || (pred> predecessor && pred< currNode) then
                 predecessor <- pred
 
         | TellState ->
-            mailbox.Sender <! state
+            mailbox.Sender() <! CheckPred(state)
          
         | CheckPred(state) ->
             if state = "failed" then
-                predecessor <- -1
+                predecessor <- ""
             
         | FixFingers ->
             next <- next + 1
             if next >= hash_length then
-                next <- 1             
-            let power = (next-1) |>bigint
-            let addition:bigint = nodeInt + bigint(2.0 ** power) 
-            let id =  (addition % maxNode)
-            mailbox.Self <! FindSuccessor(id, nodeInt, next)
+                next <- 1         
+         
+            let power = (next-1) |> float
+            let addition = (nodeInt + bigint(2.0 ** power))
+            let finger_int_val =  (addition % maxNode)
+            let mutable finger_val = finger_int_val.ToString("X")
+            if finger_val.Length < 41 then
+                finger_val <- (String.replicate (41-finger_val.Length) "0") + finger_val
+            mailbox.Self <! FindSuccessor(finger_val, currNode, next)
         
         | UpdateFinger(id, index) ->
+            // printfn "Update finger of %i as %s" index id
             finger_table.[index] <- id
 
         // | Begin_Simulation ->
@@ -219,7 +203,7 @@ let Chord_Node (mailbox : Actor<_>) =
         //         // else
         //             //if successor has key
         //         let successor = finger_table.[0]
-        //         if (hashedMsg > nodeInt && hashedMsg <= successor) || (successor< nodeInt && (hashedMsg> nodeInt || hashedMsg<= successor)) then
+        //         if (hashedMsg > currNode && hashedMsg <= successor) || (successor< currNode && (hashedMsg> currNode || hashedMsg<= successor)) then
         //             let path = basePath+successor
         //             let actorRef = select path chord_system
         //             mailbox.Context.Parent <! Received_Message(1)
@@ -238,7 +222,7 @@ let Chord_Node (mailbox : Actor<_>) =
         //     let mutable num_hops = hop + 1
          
         //     let successor = finger_table.[0]
-        //     if (hashedMsg > nodeInt && hashedMsg <= successor) || (successor< nodeInt && (hashedMsg> nodeInt || hashedMsg<= successor)) then
+        //     if (hashedMsg > currNode && hashedMsg <= successor) || (successor< currNode && (hashedMsg> currNode || hashedMsg<= successor)) then
         //         let path = basePath+successor
         //         let actorRef = select path chord_system 
         //         mailbox.Context.Parent <! Received_Message(num_hops+1)
@@ -254,7 +238,9 @@ let Chord_Node (mailbox : Actor<_>) =
         //         // printfn "routing %s to %s" hashedMsg path
         //         // printfn "send key to next closest preceeding node %s %s" hashedMsg next_node
         //         actorRef <! Route(hashedMsg, num_hops)
-                
+
+        chord_system.Scheduler.ScheduleTellOnce(TimeSpan.FromSeconds(1.0), mailbox.Self, TellYourPred, mailbox.Self)         
+        chord_system.Scheduler.ScheduleTellOnce(TimeSpan.FromSeconds(1.0), mailbox.Self, FixFingers, mailbox.Self)
         return! loop()
     }
     loop()
@@ -262,8 +248,9 @@ let Chord_Node (mailbox : Actor<_>) =
 let Supervisor (mailbox : Actor<_>) =
     let mutable init_count = 0
     let mutable node_count = 0
-    let mutable chord_list = new List<IActorRef>()
-    let firstNode:bigint = 0 |>bigint
+    // let mutable chord_list = new List<IActorRef>()
+    let mutable firstNode = ""
+
     let total_requests = num_nodes * num_message
     printfn "total requests = %i" total_requests
     let mutable average_hops = 0
@@ -272,25 +259,21 @@ let Supervisor (mailbox : Actor<_>) =
         let! message = mailbox.Receive()
         match message with
         | Start(nodes, messages) -> 
-            let prefix = "chord_node_"
-            for i in 1 .. nodes do
+            let nodeName = ranStr 10
+            let hashedValue = hash_string nodeName
+            printfn "First node %s" hashedValue
+            firstNode <- hashedValue
+            let actorRef = spawn mailbox.Context hashedValue chordNode
+            actorRef <! Create
+            
+
+        | CreateOtherNodes ->
+            for i in 1 .. num_nodes-1 do
                 let nodeName = ranStr 10
                 let hashedValue = hash_string nodeName
-                let intValue = convertToBigInt hashedValue
-                let actorRef = spawn mailbox.Context hashedValue (actorOf2 Chord_Node)
-                if i=1 then
-                    firstNode <- intValue
-                    actorRef <! Create
-                else                                  
-                    actorRef <! Join(firstNode)
-                
-
-        | Init_Done ->  init_count <- init_count + 1
-                        if init_count = node_count then
-                            printfn "Chord ring created"
-                            chord_list |> Seq.iteri (fun i chord_node -> 
-                                    chord_node <! Begin_Simulation)
-                            // chord_list.[9] <! Begin_Simulation
+                let actorRef = spawn mailbox.Context hashedValue chordNode
+                actorRef <! Join(firstNode)
+      
         | Received_Message(hops) ->
             count <- count + 1
             average_hops <- average_hops + hops
--- Project 3/test.fsx
+++ Project 3/test.fsx
@@ -12,27 +12,37 @@ let hash_string (input: string, algo: string) =
                         |> Seq.reduce (+))
     hash_string
 
-let blah = "sjdhbsbd"
+// let blah = "sjdhbsbd"
 
-let hash_type = "SHA1"
-let hash = hash_string (blah, hash_type)
-printfn "%s" hash
-let num =  bigint.Parse(hash, System.Globalization.NumberStyles.HexNumber)
-printfn "%O" num
-let backToHex = num.ToString "x2"
-printfn "%s" backToHex
-printfn "%i" backToHex.Length
+// let hash_type = "SHA1"
+// let hash = hash_string (blah, hash_type)
+// printfn "%s" hash
+// let num =  bigint.Parse(hash, System.Globalization.NumberStyles.HexNumber)
+// printfn "%O" num
+// let backToHex = num.ToString "x2"
+// printfn "%s" backToHex
+// printfn "%i" backToHex.Length
 
-// let mutable num : bigint = bigint(2.0** 160.0) 
-// num <- (num - bigint(1.0))
-// let hash = num.ToString("X")
-// let num2 = bigint(2.0)
-// let hash0 = num2.ToString("x2")
-// let num3 =  bigint(2.0**54.0)
-// let hash3 = num3.ToString("X")
-// let num4 = bigint(2.0**101.0)
-// let hash4 = num4.ToString("X")
+let mutable num : bigint = bigint(2.0** 160.0) 
+num <- (num - bigint(1.0))
+let hash = num.ToString("X")
+let num2 = bigint(2.0)
+let hash0 = num2.ToString("x2")
+let num3 =  bigint(2.0**54.0)
+let mutable hash3 = num3.ToString("X")
+let num4 = bigint(2.0**101.0)
+let mutable hash4 = num4.ToString("X")
+if hash4.Length < 41 then
+    hash4 <- (String.replicate (41-hash4.Length) "0") + hash4
 
-// printfn "Length of %s is %i" hash3 hash.Length 
-// printfn "Length of %s is %i" hash4 hash4.Length 
-// printfn "%b" (hash3>hash4)
+printfn "Length of %s is %i" hash3 hash.Length 
+printfn "Length of %s is %i" hash4 hash4.Length 
+printfn "%b" (hash3>hash4)
+
+let next = (101-1) |>float
+let power = 2.0 ** next
+let addition = (num3 + bigint(2.0 ** next))
+
+let finger_int_val =  (addition % num)
+let mutable finger_val = finger_int_val.ToString("X")
+printfn "%s %i" finger_val finger_val.Length
